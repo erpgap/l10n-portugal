18e62bea8dd4ad343444aaf4d33f9e920287a8e9
Diogo Duarte <dduarte@erpgap.com>
2025-02-25 13:10:09 +0000

    [IMP] __manifest__.py

---
+++ l10n_pt_account_toconline/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2025 ERPGAP
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Portugal TOCOnline Integration",
+    "summary": "Portuguese certified invoices using TOCOnline",
+    "version": "18.0.0.0.1",
+    "author": "ERPGAP, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/l10n-portugal",
+    "category": "Accounting/Localizations/EDI",
+    "maintainers": ["dduarte-odoogap"],
+    "development_status": "Alpha",
+    "depends": ["l10n_pt_vat", "account"],
+    "data": [
+    ],
+    "images": ["static/description/cover.png"],
+    "application": True,
+    "installable": True,
+}
